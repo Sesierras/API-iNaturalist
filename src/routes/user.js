51276e5e9ca27a2af61fbb8842b05c761ebd9a51
sesierras <sesierras@unal.edu.co>
2023-02-23 15:02:03 -0500

    Add some endpoints with specific routes

--- src/routes/user.js
+++ src/routes/user.js
@@ -4,45 +4,79 @@ const userSchema = require("../models/user");
 
 /*<<<<<<<<<<<<<<Routes>>>>>>>>>>>>>>>*/
 
-/*<<New observation>>*/
-router.post("/users", (req, res) => {
-	const user = new userSchema(req.body);
-	user
-	.save()
-	.then(
-		(data) => res.json(data),
-		log("✅ New observation added to MongoDB-Atlas")
-		)
-		.catch((error) => res.json({ message: error.message }));
-	});
-
-/*<<Get an specific # of observations>>*/
-// router.get("/users", (req, res) => {
-//   userSchema
-//     .find()
-//     .limit(1)            //limit the number of observations to be returned
-//     .sort({ date: -1 })                //sort the observations by date in descending orders
-//     .then((data) => {
-//       res.status(200).json(data); // Status code for OK (200)
-//       console.log("✅ All observations retrieved from MongoDB-Atlas");
-//     })
-//     .catch((error) => {
-//       res.status(500).json({ message: "Error retrieving observations" }); // Status code for Internal Server Error (500)
-//     });
-// });
 
 module.exports = router;
 
-router.get("/users", async (req, res) => {
-  try {
-    const data = await userSchema
+/*<<New observation>>*/
+router.post("/users/", (req, res) => {
+  const user = new userSchema(req.body);
+	user
+  .save()
+  .then(
+    (data) => res.json(data),
+    console.log("✅ New observation added to MongoDB-Atlas")
+    )
+    .catch((error) =>
+    res.status(500).json({ message: "Error adding observation" })
+    );
+	});
+
+  /*<<Get an specific number of observations>>*/
+  router.get("/users", async (req, res) => {
+    try {
+      const data = await userSchema
       .find()
-      .limit(1) // limit the number of observations to be returned
+      .limit(10) // limit the number of observations to be returned
       .sort({ date: -1 }); // sort the observations by date in descending orders
-    res.status(200).json(data); // Status code for OK (200)
+      res.status(200).json(data); // Status code for OK (200)
     console.log("✅ All observations retrieved from MongoDB-Atlas");
   } catch (error) {
     res.status(500).json({ message: "Error retrieving observations" }); // Status code for Internal Server Error (500)
   }
 });
-module.exports = router;
+
+/*<<Get an specific number of observations>>*/
+router.get("/users/id/:id", (req, res) => {
+  const {id}= req.params;
+  userSchema
+  .findById(id)
+  .then((data) => res.status(200).json(data), console.log("✅ Observation retrieved from MongoDB-Atlas"))
+  .catch((err) => res.status(404).json({ message: "Observation not found"}))
+});
+
+
+router.get("/users/catalogNumber/:catalogNumber", (req, res) => {
+  const { catalogNumber } = req.params;
+  userSchema
+    .findOne({ catalogNumber })
+    .then(
+      (data) => res.status(200).json(data),
+      console.log("✅ Observation retrieved from MongoDB-Atlas")
+    )
+    .catch((err) => res.status(404).json({ message: "Observation not found" }));
+});
+/*<<Update an observation>>*/
+
+router.get("/users/countryCode/:countryCode", async (req, res) => {
+  try {
+    const data = await userSchema
+      .find({ countryCode: req.params.countryCode })
+      .limit(10) // limit the number of observations to be returned
+      .sort({ date: -1 }); // sort the observations by date in descending order
+
+    if (data.length === 0) {
+      // Si no se encontró ninguna observación con ese countryCode, devuelve un error 404
+      return res
+        .status(404)
+        .json({ message: "No observations found in this Country" });
+    }
+
+    console.log(
+      `✅ Observations in ${req.params.countryCode} retrieved from MongoDB-Atlas`
+    );
+    res.json(data);
+  } catch (error) {
+    console.error(error);
+    res.status(500).json({ message: "Error retrieving observations" });
+  }
+});
